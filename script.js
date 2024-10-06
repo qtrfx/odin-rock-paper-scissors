2d3ec92b97f4fdc9dae5315541fe6b384f3b25a0
qtrfx <24359705+qtrfx@users.noreply.github.com>
2024-10-07 01:46:37 +0200

    feat: add logic to get human input

--- script.js
+++ script.js
@@ -14,3 +14,27 @@ function getComputerChoice() {
     else if (randomRoll === 2) return "scissors";
 }
 
+function getHumanChoice() {
+    let invalidChoice = false;
+    while (true) {
+        let userPrompt = invalidChoice
+            ? "Please pick a valid entry:\n1. Rock\n2. Paper\n3. Scissors"
+            : "Pick one:\n1. Rock\n2. Paper\n3. Scissors";
+
+        let userInput = prompt(userPrompt);
+        invalidChoice = false;
+        switch (userInput.toLowerCase()) {
+            case "1":
+            case "rock":
+                return "rock";
+            case "2":
+            case "paper":
+                return "paper";
+            case "3":
+            case "scissors":
+                return "scissors";
+            default:
+                invalidChoice = true;
+        }
+    }
+}
