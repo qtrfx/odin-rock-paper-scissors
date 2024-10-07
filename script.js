cd9b5b542959642f3300e8b59debeeec2fa1f485
qtrfx <24359705+qtrfx@users.noreply.github.com>
2024-10-07 10:06:03 +0200

    refactor: put needed functions into playGame scope

--- script.js
+++ script.js
@@ -19,28 +19,69 @@ function playGame() {
         humanScore > computerScore ? "The player wins!" : "The computer wins!"
     );
 
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice === "rock") {
+            if (computerChoice === "rock") {
+                console.log("It's a draw!");
+            } else if (computerChoice === "paper") {
+                console.log(`You lose! Paper beats Rock!`);
+                computerScore++;
+            } else {
+                console.log("You win! Rock beats Scissors!");
+                humanScore++;
+            }
+        } else if (humanChoice === "paper") {
+            if (computerChoice === "rock") {
+                console.log("You win! Paper beats Rock!");
+                humanScore++;
+            } else if (computerChoice === "paper") {
+                console.log("It's a draw!");
+            } else if (computerChoice === "scissors") {
+                console.log("You lose! Scissors beat Paper!");
+                computerScore++;
+            }
+        } else {
+            if (computerChoice === "rock") {
+                console.log("You lose! Rock beats Scissors!");
+                computerScore++;
+            } else if (computerChoice === "paper") {
+                console.log("You win! Scissors beat Paper!");
+                humanScore++;
+            } else if (computerChoice === "scissors") {
+                console.log("It's a draw!");
+            }
+        }
+    }
+    function getComputerChoice() {
+        const randomRoll = Math.floor(Math.random() * 3);
 
-function getHumanChoice() {
-    let invalidChoice = false;
-    while (true) {
-        let userPrompt = invalidChoice
-            ? "Please pick a valid entry:\n1. Rock\n2. Paper\n3. Scissors"
-            : "Pick one:\n1. Rock\n2. Paper\n3. Scissors";
+        if (randomRoll === 0) return "rock";
+        else if (randomRoll === 1) return "paper";
+        else if (randomRoll === 2) return "scissors";
+    }
 
-        let userInput = prompt(userPrompt);
-        invalidChoice = false;
-        switch (userInput.toLowerCase()) {
-            case "1":
-            case "rock":
-                return "rock";
-            case "2":
-            case "paper":
-                return "paper";
-            case "3":
-            case "scissors":
-                return "scissors";
-            default:
-                invalidChoice = true;
+    function getHumanChoice() {
+        let invalidChoice = false;
+        while (true) {
+            let userPrompt = invalidChoice
+                ? "Please pick a valid entry:\n1. Rock\n2. Paper\n3. Scissors"
+                : "Pick one:\n1. Rock\n2. Paper\n3. Scissors";
+
+            let userInput = prompt(userPrompt);
+            invalidChoice = false;
+            switch (userInput.toLowerCase()) {
+                case "1":
+                case "rock":
+                    return "rock";
+                case "2":
+                case "paper":
+                    return "paper";
+                case "3":
+                case "scissors":
+                    return "scissors";
+                default:
+                    invalidChoice = true;
+            }
         }
     }
 }
